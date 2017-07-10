e64f8102deec0d7bd4d5bbbfbfe339d59ccc7b2d
matthew hallberg <matthewhallberg@gmail.com>
2017-07-10 01:36:33 -0400

    Added pokemon in gym with attacks and behaviors, added pikachu with idle animation and electric shock.

---
+++ Assets/Scripts/LaprasBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaprasBehavior : MonoBehaviour {
+
+	public AnimationClip idle, attack;
+	public ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+		ps.Stop ();
+		StartCoroutine (Behavior ());
+	}
+	
+	IEnumerator Behavior(){
+		while (true) {
+			GetComponent<Animation> ().clip = idle;
+			GetComponent<Animation> ().Play ();
+			yield return new WaitForSeconds (3f);
+			GetComponent<Animation> ().clip = attack;
+			GetComponent<Animation> ().Play ();
+			yield return new WaitForSeconds (.8f);
+			GetComponent<AudioSource> ().Play ();
+			yield return new WaitForSeconds (.2f);
+			ps.Play ();
+			yield return new WaitForSeconds (1f);
+			ps.Stop ();
+			yield return new WaitForSeconds (1f);
+		}
+	}
+}
---
+++ Assets/Scripts/PikachuBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PikachuBehavior : MonoBehaviour {
+
+	public AnimationClip idle, walk;
+	public GameObject electric;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Behavior ());
+		GetComponent<Animation> ().clip = idle;
+		GetComponent<Animation> ().Play ();
+	}
+
+	IEnumerator Behavior(){
+		while (true) {
+			electric.SetActive (true);
+			GetComponent<AudioSource> ().Play ();
+			yield return new WaitForSeconds (1f);
+			electric.SetActive (false);
+			yield return new WaitForSeconds (3f);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
